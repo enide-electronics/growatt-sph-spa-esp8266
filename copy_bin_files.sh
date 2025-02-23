4baae9af241d80bd053d0f06df58994740c30ada
joao <jfigueiredo@enide.net>
2025-02-23 18:45:48 +0000

    Change captive/web portal title

--- copy_bin_files.sh
+++ copy_bin_files.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp .pio/build/wemos_d1_mini_4m/firmware.bin src/bin/growatt-sph-spa-esp8266.ino.d1_mini.bin
 cp .pio/build/esp01_1m/firmware.bin src/bin/growatt-sph-spa-esp8266.ino.generic.bin
+
